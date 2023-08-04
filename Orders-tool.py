b0904bd98458fc879e844db7600d821bdb40eb65
Sujith Digajerla <81622472+sujith-kumar-digajerla@users.noreply.github.com>
2023-08-05 02:01:38 +0530

    Update Orders-tool.py

--- Orders-tool.py
+++ Orders-tool.py
@@ -1,20 +1,40 @@
 import datetime as dt
 import streamlit as st
 import snowflake.connector
-import pandas as pdt
 
-st.title('Orders tool')
-date = st.date_input("🗓date:", dt.date(2023, 1, 1))
-BA = st.text_input("Business Area:")
-Division = st.text_input("Division:")
-Orders = st.text_input("Orders:")
+st.title('🧾 Orders tool 🧾')
+
+date_, BA_, Division_, Orders_, submit_ = st.columns(5, gap="small")
+
+with date_:
+    date = st.date_input("📅 Date:", dt.date(2023, 1, 1))
+with BA_:
+    BA = st.selectbox("🏛️ Business Area:", ("EL", "MO", "PA", "RA"))
+# BA = st.text_input("Business Area:")
+with Division_:
+    if BA == "EL":
+        Division = st.selectbox("🏛️ Division:", ("ELDS", "ELIP", "ELSE", "ELSB", "ELSP"))
+    elif BA == "PA":
+        Division = st.selectbox("🏛️ Division:", ("PAEN", "PAPI", "PAMP", "PAMA"))
+    elif BA == "MO":
+        Division = st.selectbox("🏛️ Division:", ("MODP", "MOLM", "MOLMG", "MONM", "MOMS", "MOSD", "MOT"))
+    elif BA == "RA":
+        Division = st.selectbox("🏛️ Division:", ("RARO", "RAMA"))
+
+# Division = st.text_input("Division:")
+with Orders_:
+    Orders = st.text_input("💲 Orders:")
+
 date_str = str(date)
 cnx = snowflake.connector.connect(**st.secrets["snowflake"])
-if st.button('Submit'):
-    cur = cnx.cursor()
-    cur.execute(
-        "insert into USERINPUTDB.OR_REV_SCHEMA.Orders_tbl values ('" + date_str + "' , '" + BA + "' , '" + Division + "' , '" + Orders + "' ) ;")
-    st.text(' Orders value added !')
+
+with submit_:
+    if st.button('Submit'):
+        cur = cnx.cursor()
+        cur.execute(
+            "insert into USERINPUTDB.OR_REV_SCHEMA.Orders_tbl values ('" + date_str + "' , '" + BA + "' , '" + Division + "' , '" + Orders + "' ) ;")
+        st.text('Submitted !')
+
 
 if st.button('Show Data'):
     cur = cnx.cursor()
